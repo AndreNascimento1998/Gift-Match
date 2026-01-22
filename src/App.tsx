fcc2d692e9f09253af4c8fcdfb43e6e35c2f5642
Andre <andre.ncardoso@hotmail.com>
2026-01-22 18:58:27 -0300

    feat: add styling and routing components, implement tests for App and Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom"
+import "./App.scss"
 
 function App() {
     return (
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import App from "@/App"
-import Home from "@/pages/Home"
-import About from "@/pages/About"
+import Home from "@/pages/Home/Home"
+import About from "@/pages/About/About"
 
 const router = createBrowserRouter([
     {

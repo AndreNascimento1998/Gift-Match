ef5c09705d6785876ac8fd326740924e50d115df
Andre <andre.ncardoso@hotmail.com>
2026-01-26 19:07:40 -0300

    style: update HeaderVisitor component with new typography classes and background color adjustments

--- src/pages/HeaderVisitor/Index.tsx
+++ src/pages/HeaderVisitor/Index.tsx
@@ -8,21 +8,21 @@ const HeaderVisitor = () => {
     const { toggleTheme } = headerHook
 
     return (
-        <header className="flex justify-between items-center py-4 px-8 rounded-lg bg-background-component">
+        <header className="flex justify-between items-center py-4 px-8 rounded-lg bg-background-default">
             <section className="hidden md:flex items-center gap-2">
-                <h1>Amigo secreto online </h1>
+                <h1 className="text-h3">Amigo secreto online </h1>
                 <GiftIcon
                     onClick={toggleTheme}
-                    className="bg-background-component"
+                    className="bg-background-default"
                 />
             </section>
             <article className="hidden lg:flex gap-9">
-                <span>Como funciona</span>
-                <span>Privacidade</span>
+                <span className="text-h3">Como funciona</span>
+                <span className="text-h3">Privacidade</span>
             </article>
             <section
                 onClick={toggleTheme}
-                className="block md:hidden bg-background-component"
+                className="block md:hidden bg-background-default"
             >
                 Avatar
             </section>

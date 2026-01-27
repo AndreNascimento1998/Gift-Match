52151acda626b23fa347b19615e964587920eeb4
Andre <andre.ncardoso@hotmail.com>
2026-01-27 16:12:24 -0300

    style: update pre-commit script to run format:check and lint before tests; refactor LastStep component for improved readability

--- src/pages/RegisterPage/componets/LastStep.tsx
+++ src/pages/RegisterPage/componets/LastStep.tsx
@@ -5,16 +5,18 @@ type LastStepProps = {
     setCurrentStep: React.Dispatch<React.SetStateAction<number>>
 }
 
-const LastStep = ({currentStep, setCurrentStep}: LastStepProps) => {
-    return <div>
-        <CustomComponent 
-            title="Última etapa:"
-            subtitle="Esses dados serão usados para criar o grupo e enviar os convites."
-            buttonText="Finalizar grupo"
-            currentStep={currentStep}
-            setCurrentStep={setCurrentStep}
-        />
-    </div>
+const LastStep = ({ currentStep, setCurrentStep }: LastStepProps) => {
+    return (
+        <div>
+            <CustomComponent
+                title="Última etapa:"
+                subtitle="Esses dados serão usados para criar o grupo e enviar os convites."
+                buttonText="Finalizar grupo"
+                currentStep={currentStep}
+                setCurrentStep={setCurrentStep}
+            />
+        </div>
+    )
 }
 
 export default LastStep
